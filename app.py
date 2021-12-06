c43aea3d39cc744b951066fbcb70a0b3c803553f
Preeti Amin <Preeti.amin@outlook.com>
2021-12-06 13:13:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import surprise
+from surprise import SVD
+from surprise import Dataset
+from surprise import Reader
 
 
 
@@ -9,8 +11,9 @@ HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; borde
 
 st.title('Movie Recommendation System')
 
-selected_type = st.radio('Recommendation Type', ('Top Movies by Genre', 'Collaborative Filtering'))
-
+selected_type = st.sidebar.radio(
+    "Recommendation Type",
+    ("Top Movies by Genre", "Collaborative Filtering"))
 
 genre_list = ["Action", "Adventure", "Animation", 
                "Children's", "Comedy", "Crime",
@@ -45,35 +48,88 @@ def load_data():
 
 movies = load_data()
 
-def get_top_movies_by_rating(genre):
-    return movies[(movies['AveRating']>2) & (movies[genre]==1)].sort_values('AveRating', \
-    ascending=False)[['MovieID','Title','AveRating']][:5]
+def get_top_movies_by_rating(genre, n):
+    return movies[(movies['NumRatings']>100) & (movies[genre]==1)].sort_values('AveRating', \
+    ascending=False)[['MovieID','Title','AveRating']][:n]
  
-def get_top_movies_by_popularity(genre):
-    return movies[(movies['NumRatings']>100) & (movies[genre]==1)].sort_values('NumRatings', \
-    ascending=False)[['MovieID','Title','AveRating']][:5]
+def get_top_movies_by_popularity(genre, n):
+    return movies[(movies['AveRating']>2) & (movies[genre]==1)].sort_values('NumRatings', \
+    ascending=False)[['MovieID','Title','NumRatings']][:n]
+  
+def get_popular_movies(n):
+    return movies.sort_values('NumRatings', ascending=False)[['MovieID','Title','AveRating']][:n]
+
+def get_random_movies(n):
+    return movies.sample(n)
 
 
 if selected_type=='Top Movies by Genre':
                                                 
-    selected_method = st.radio('Rating Method', ('By User Rating', 'By Popularity'))
-
+    selected_method = st.sidebar.radio('Rating Method', ('By User Rating', 'By Popularity'))
 
     selected_genre = st.selectbox('Genre',genre_list,index=0)
-
+    n=10
+    
     if selected_method =='By User Rating':
-        top_movies=get_top_movies_by_rating(selected_genre)
+        top_movies=get_top_movies_by_rating(selected_genre, n)
     elif selected_method =='By Popularity':
-        top_movies=get_top_movies_by_popularity(selected_genre)
+        top_movies=get_top_movies_by_popularity(selected_genre, n)
 
+    rows = n//5
     cols = st.columns(5)
-    #st.write(img_url+'/'+str(top_movies.iloc[1,0])+'.jpg')
     i=0
-    for col in cols:
-        with col:
-            st.image(img_url+'/'+str(top_movies.iloc[i,0])+'.jpg',width=100)
-            st.write(top_movies.iloc[i,1][:-6])
-            i+=1
+    for row in range(rows+1):
+      for col in cols:
+          with col:
+              st.image(img_url+'/'+str(top_movies.iloc[i,0])+'.jpg',width=100)
+              #st.write(top_movies.iloc[i,1][:-6])
+              i+=1
+              
+elif selected_type=='Collaborative Filtering':
+                                                
+    selected_method = st.sidebar.radio('Rating Method', ('User Based', 'Item Based', 'SVD'))
+    n=10
+
+    user_item = ratings.pivot_table('Rating','UserID','MovieID')
+    user_item.fillna(0,inplace=True)
+    reader = Reader(rating_scale=(1, 5))
+
+    data = Dataset.load_from_df(ratings[['UserID', 'MovieID', 'Rating']], reader)
+
+    # Use the famous SVD algorithm.
+    algo = SVD()
+
+    st.caption('Rate some movies first, then click Submit to get recommendations')
+
+    movies_to_rate = get_popular_movies(50)
+    rows = 10
+    
+    with st.form(key='columns_in_form'):
+        cols = st.columns(5)
+        #st.write(img_url+'/'+str(top_movies.iloc[1,0])+'.jpg')
+        i=0
+        ratings = []
+        for row in range(1,rows+1):
+            for col in cols:
+                with col:
+                    st.image(img_url+'/'+str(movies_to_rate.iloc[i,0])+'.jpg',width=100)
+                    val = col.selectbox(f'', ['Not Rated','1','2','3','4','5'], key=i)
+                    user_item.loc[9999,movies_to_rate.iloc[i,0]]=val
+                    i+=1
+        submitted = st.form_submit_button('Submit')
+        if submitted:
+            st.write(user_item)
+            #for i in range(rows*cols+1):
+
+    '''
+    if selected_method =='By User Rating':
+        top_movies=get_top_movies_by_rating(selected_genre, n)
+    elif selected_method =='By Popularity':
+        top_movies=get_top_movies_by_popularity(selected_genre, n)
+   '''
+
+
+
 
 
 
